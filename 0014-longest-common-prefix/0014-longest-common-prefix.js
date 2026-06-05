3a1f2c35b937f2e795413b700632f5dd2854b395
Akkunur Nithin <akkunurnithin1234@gmail.com>
2026-06-05 21:54:16 +0530

    Time: 0 ms (100%), Space: 54.1 MB (63.77%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -3,17 +3,13 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-     if (strs.length === 0) return "";
+     let prefix = strs[0];
 
-    for (let i = 0; i < strs[0].length; i++) {
-        let char = strs[0][i];
-
-        for (let j = 1; j < strs.length; j++) {
-            if (strs[j][i] !== char) {
-                return strs[0].slice(0, i);
-            }
+    for (let i = 1; i < strs.length; i++) {
+        while (!strs[i].startsWith(prefix)) {
+            prefix = prefix.slice(0, -1);
         }
     }
 
-    return strs[0];
+    return prefix;
 };
