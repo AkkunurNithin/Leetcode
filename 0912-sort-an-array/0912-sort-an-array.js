57e3e4077543bd376831df5cfade0096090e95da
Akkunur Nithin <akkunurnithin1234@gmail.com>
2026-06-15 12:10:26 +0530

    Time: 191 ms (16.51%), Space: 80.8 MB (48.88%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function(nums) {
+   
+    if(nums.length <= 1) return nums;
+
+    let mid = Math.floor(nums.length / 2);
+    
+    const left = sortArray(nums.slice(0, mid));
+    const right = sortArray(nums.slice(mid));
+
+    return merge(left, right);
+};
+
+function merge(left, right) {
+    let result = [];
+    let i = 0, j = 0;
+
+    while (i < left.length && j < right.length) {
+        if (left[i] < right[j]) {
+            result.push(left[i++]);
+        } else {
+            result.push(right[j++]);
+        }
+    }
+
+    return result.concat(left.slice(i)).concat(right.slice(j));
+}
