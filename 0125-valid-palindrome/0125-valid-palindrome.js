da5d625e6d91cb9435506064f7b7b7dfea2ff2c0
Akkunur Nithin <akkunurnithin1234@gmail.com>
2026-06-23 19:01:31 +0530

    Time: 5 ms (64.4%), Space: 56.1 MB (88.34%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,24 +3,18 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
+     let str = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+
     let left = 0;
-    let right = s.length-1;
+    let right = str.length - 1;
 
-    while(left<right){
-         while (left < right && !/[a-zA-Z0-9]/.test(s[left])) {
-            left++;
-        }
-
-        while (left < right && !/[a-zA-Z0-9]/.test(s[right])) {
-            right--;
-        }
-
-        if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+    while (left < right) {
+        if (str[left] !== str[right]) {
             return false;
         }
-
         left++;
         right--;
     }
+
     return true;
 };
