3525ea8883beea686a27a7fd3f1145b865c4ee49
Akkunur Nithin <akkunurnithin1234@gmail.com>
2026-07-06 15:35:27 +0530

    Time: 1 ms (76.53%), Space: 58.3 MB (96.68%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,13 +3,20 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let j = 0;
+     let index = 0;
 
+    // Move all non-zero elements to the front
     for (let i = 0; i < nums.length; i++) {
-       if(nums[i] !== 0){
-        [nums[i],nums[j]] = [nums[j],nums[i]];
-        j++;
-       }
+        if (nums[i] !== 0) {
+            nums[index] = nums[i];
+            index++;
+        }
+    }
+
+    // Fill remaining positions with zeros
+    while (index < nums.length) {
+        nums[index] = 0;
+        index++;
     }
 
     return nums;
