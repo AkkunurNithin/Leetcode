8d8bee9fa677976d39b03220404ba6d1f065f738
Akkunur Nithin <akkunurnithin1234@gmail.com>
2026-07-16 00:44:26 +0530

    Time: 1088 ms (12.4%), Space: 57.7 MB (88.65%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+     for(let row of matrix){
+
+         if (target < row[0] || target > row[row.length - 1]) {
+            continue;
+        }
+
+        let left = 0;
+        let right = row.length-1;
+
+        while(left <= right){
+            let mid = Math.floor((left+right)/2);
+
+            if(row[mid] === target){
+                return true;
+            }
+            if(row[mid] < target){
+                left = mid+1;
+            }else{
+                right = mid-1;
+            }
+        }
+    }
+        return false;
+};
