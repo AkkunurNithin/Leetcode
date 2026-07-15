055a50400707414f4daaa18ef0a4ab5df5e02a51
Akkunur Nithin <akkunurnithin1234@gmail.com>
2026-07-16 00:12:24 +0530

    Time: 21 ms (56.53%), Space: 54.6 MB (85.47%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+     if (s.length < 2) return s;
+
+    let start = 0;
+    let maxLen = 1;
+
+    function expand(left, right) {
+        while (
+            left >= 0 &&
+            right < s.length &&
+            s[left] === s[right]
+        ) {
+            if (right - left + 1 > maxLen) {
+                maxLen = right - left + 1;
+                start = left;
+            }
+            left--;
+            right++;
+        }
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        // Odd length palindrome
+        expand(i, i);
+
+        // Even length palindrome
+        expand(i, i + 1);
+    }
+
+    return s.substring(start, start + maxLen);
+    }
